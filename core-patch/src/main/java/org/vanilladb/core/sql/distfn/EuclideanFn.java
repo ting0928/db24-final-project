a9b993aefdf7de74ca57c29eb5e076fb01320172
ting0928 <73580961+ting0928@users.noreply.github.com>
2024-06-16 16:52:17 +0800

    Finish EuclideanFn SIMD

--- core-patch/src/main/java/org/vanilladb/core/sql/distfn/EuclideanFn.java
+++ core-patch/src/main/java/org/vanilladb/core/sql/distfn/EuclideanFn.java
@@ -1,21 +1,35 @@
 package org.vanilladb.core.sql.distfn;
 
 import org.vanilladb.core.sql.VectorConstant;
+import jdk.incubator.vector.FloatVector;
+import jdk.incubator.vector.VectorOperators;
+import jdk.incubator.vector.VectorSpecies;
 
 public class EuclideanFn extends DistanceFn {
-
+    static final VectorSpecies<Float> SPECIES = FloatVector.SPECIES_PREFERRED;
     public EuclideanFn(String fld) {
         super(fld);
     }
 
     @Override
     protected double calculateDistance(VectorConstant vec) {
-        double sum = 0;
-        for (int i = 0; i < vec.dimension(); i++) {
-            double diff = query.get(i) - vec.get(i);
-            sum += diff * diff;
+        int i = 0;
+        FloatVector sum = FloatVector.zero(SPECIES);
+        for (; i < SPECIES.loopBound(vec.dimension()); i += SPECIES.length()) {
+            FloatVector v = FloatVector.fromArray(SPECIES, vec.asJavaVal(), i);
+            FloatVector q = FloatVector.fromArray(SPECIES, query.asJavaVal(), i);
+            FloatVector diff = v.sub(q);
+            sum = diff.fma(diff, sum);
         }
-        return Math.sqrt(sum);
+        double sum_d = sum.reduceLanes(VectorOperators.ADD);
+    
+        // deal with tail of dim % SPECIES.length()
+        for (i=0 ; i < vec.dimension(); i++) {
+            double diff = query.get(i) - vec.get(i);
+            sum_d += diff * diff;
+        }
+        
+        return Math.sqrt(sum_d);
     }
     
 }
